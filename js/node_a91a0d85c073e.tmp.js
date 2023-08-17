f977693135d46bfaca8c5f3f5edf5c0aecab2360
ALEXANDRE ARMELLINE DA SILVA <alexandrearmelline@gmail.com>
2023-08-16 21:20:07 -0300

    inicio modulo 4

--- js/node_a91a0d85c073e.tmp.js
+++ js/node_a91a0d85c073e.tmp.js
@@ -5,13 +5,15 @@ const braco = document.querySelector("#braco")
 const controle = document.querySelectorAll(".controle-ajuste")
 
 controle.forEach((elemeto)=>{
-    elemeto.addEventListener("click", (evento) =>{
-        manipulaDados(evento.target.textContent)
-    } )
+    console.log(elemeto)
 } )
 
+somar.addEventListener("click", ()=> {manipulaDados("somar")})
+
+subtrair.addEventListener("click", ()=> {manipulaDados("subtrair")})
+
 function manipulaDados(operacao){
-    if(operacao === "-") {
+    if(operacao === "subtrair") {
         braco.value = parseInt(braco.value) - 1
     } else {
         braco.value = parseInt(braco.value) + 1
